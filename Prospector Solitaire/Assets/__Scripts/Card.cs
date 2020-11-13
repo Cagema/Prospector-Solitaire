e37e72e4b2fc04ac123896b568d23969ca9c6791
Cagema <cagema.erm@gmail.com>
2020-11-13 14:27:47 +0300

    Preparing sprites

---
+++ Prospector Solitaire/Assets/__Scripts/Card.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    
+}
+
+[System.Serializable]
+public class Decorator
+{
+    // Этот класс хранит информацию из DeckXML о каждом значке на карте
+    public string type;
+    public Vector3 loc;
+    public bool flip = false;
+    public float scale = 1f;
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+    // Этот класс хранит информацию о достоинстве карты
+    public string face; // Спрайт лицевой стороны карты
+    public int rank; // Достоинство карты (1-13)
+    public List<Decorator> pips = new List<Decorator>(); // Значки
+}
