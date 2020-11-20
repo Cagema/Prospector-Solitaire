ab24b595d561a117f14c86e4a51cfe8c80db5e20
Cagema <cagema.erm@gmail.com>
2020-11-20 15:31:55 +0300

    Init commit
    Added scripts for game logic, added deck

---
+++ Prospector Solitaire/Assets/__Scripts/BartokLayout.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SlotDefinition
+{
+    public float x;
+    public float y;
+    public bool faceUp = false;
+    public string layerName = "Default";
+    public int layerID = 0;
+    public int id;
+    public List<int> hiddenBy = new List<int>();
+    public float rot;
+    public string type = "slot";
+    public Vector2 stagger;
+    public int player;
+    public Vector3 pos;
+}
+
+public class BartokLayout : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public PT_XMLReader xmlr;
+    public PT_XMLHashtable xml;
+    public Vector2 multiplier;
+
+    public List<SlotDefinition> slotDefs;
+    public SlotDefinition drawPile;
+    public SlotDefinition discardPile;
+    public SlotDefinition target;
+
+    public void ReadLayout(string xmlText)
+    {
+        xmlr = new PT_XMLReader();
+        xmlr.Parse(xmlText);
+        xml = xmlr.xml["xml"][0];
+
+        multiplier.x = float.Parse(xml["multiplier"][0].att("x"));
+        multiplier.y = float.Parse(xml["multiplier"][0].att("y"));
+
+        SlotDefinition tSD;
+        PT_XMLHashList slotX = xml["slot"];
+
+        for(int i = 0; i < slotX.Count; i++)
+        {
+            tSD = new SlotDefinition();
+            if (slotX[i].HasAtt("type"))
+            {
+                tSD.type = slotX[i].att("type");
+            }
+            else
+            {
+                tSD.type = "slot";
+            }
+
+            tSD.x = float.Parse(slotX[i].att("x"));
+            tSD.y = float.Parse(slotX[i].att("y"));
+            tSD.pos = new Vector3(tSD.x * multiplier.x, tSD.y * multiplier.y, 0);
+
+            tSD.layerID = int.Parse(slotX[i].att("layer"));
+            tSD.layerName = tSD.layerID.ToString();
+
+            switch (tSD.type)
+            {
+                case "slot":
+                    // игнорировать
+                    break;
+
+                case "drawpile":
+                    tSD.stagger.x = float.Parse(slotX[i].att("xstagger"));
+                    drawPile = tSD;
+                    break;
+
+                case "target":
+                    target = tSD;
+                    break;
+
+                case "hand":
+                    tSD.player = int.Parse(slotX[i].att("player"));
+                    tSD.rot = float.Parse(slotX[i].att("rot"));
+                    slotDefs.Add(tSD);
+                    break;
+            }
+        }
+    }
+}
